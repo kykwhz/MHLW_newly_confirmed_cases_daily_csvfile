84e08ac1577dda6f18decd06c0701a3d4e230efa
kykwhz <kykwhz@gmail.com>
2021-09-19 17:47:46 +0900

    全体的に修正

--- 6.py
+++ 6.py
@@ -1,13 +1,12 @@
 # 6.曜日算出
 
 import datetime
-
-y,m,d=input(f'日付を入力してください(入力形式は2021-01-01,2021-1-1両方可) >>> ').split('-')
-date_string=f'{int(y)}-{int(m):02d}-{int(d):02d}'
+import re
+y,m,d=re.split('[/-]',input(f'日付を入力してください(入力形式は2021-01-01,2021-1-1,両方可。区切り文字は/,-両方可) >>> '))
 
 try:
-    date=datetime.date.fromisoformat(date_string) # date_stringに格納されている入力年月日が妥当でなければexceptで処理
-    weekday={0:'月',1:'火',2:'水',3:'木',4:'金',5:'土',6:'日'}
+    date=datetime.date(int(y),int(m),int(d)) # date_stringに格納されている入力年月日が妥当でなければexceptで処理
+    weekday={0:'月',1:'火',2:'水',3:'木',4:'金',5:'土',6:'日'} # 曜日変換ディクショナリ
     print(f'{date.year}/{date.month}/{date.day}({weekday[date.weekday()]})')
 except:
     print(f'日付不当')
