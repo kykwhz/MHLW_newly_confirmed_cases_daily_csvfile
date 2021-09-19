4ae88a6ebaa247802257f8581d7a44983da0c2fc
kykwhz <kykwhz@gmail.com>
2021-09-19 16:38:08 +0900

    6.pyの修正

--- 6.py
+++ 6.py
@@ -2,16 +2,12 @@
 
 import datetime
 
-date_string=input(f'日付を入力してください(入力形式は2021-01-01) >>> ')
+y,m,d=input(f'日付を入力してください(入力形式は2021-01-01,2021-1-1両方可) >>> ').split('-')
+date_string=f'{int(y)}-{int(m):02d}-{int(d):02d}'
+
 try:
-    date=datetime.date.fromisoformat(date_string)
-    y=date.year
-    m=date.month
-    d=date.day
-    a=date.weekday()
-
+    date=datetime.date.fromisoformat(date_string) # date_stringに格納されている入力年月日が妥当でなければexceptで処理
     weekday={0:'月',1:'火',2:'水',3:'木',4:'金',5:'土',6:'日'}
-
-    print(f'{y}/{m}/{d}({weekday[a]})')
+    print(f'{date.year}/{date.month}/{date.day}({weekday[date.weekday()]})')
 except:
     print(f'日付不当')
